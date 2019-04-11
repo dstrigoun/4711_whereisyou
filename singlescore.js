477602e19fc37cbcdb813d18682aabafb998f7c7
Dasha Strigoun <dstrigoun@outlook.com>
2019-04-10 18:59:41 -0700

    start result logic

--- singlescore.js
+++ singlescore.js
@@ -1,3 +1,15 @@
+let index = parseInt(localStorage.getItem("index"));
+
 function printScore() {
     document.getElementById("score").innerHTML = "Score: " + localStorage.getItem("score");
+
+    if (index == 4) {
+        // get button by id
+        // change button text to "Go to leaderboard"
+        // onclick, change location.href = "leaderboard.html";
+    } else {
+        // get button by id
+        // change button text to "Next round"
+        // onclick, change location.href = "game.html";
+    }
 }
